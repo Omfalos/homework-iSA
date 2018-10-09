67192cfeb16913077dbd0253102f96053d5e27c4
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-10-09 17:46:17 +0200

    exe 2

--- 05/main.js
+++ 05/main.js
@@ -1,8 +1,15 @@
 function evenOrOdd(num){
     if(num % 2 == 0){
-        console.log("even")
+        console.log("even");
+        previousAndNext(num)
     }else{
         console.log("odd")
     }
 }
+
 evenOrOdd(2);
+
+function previousAndNext(num){
+    console.log(num - 1);
+    console.log(num + 1);
+}
